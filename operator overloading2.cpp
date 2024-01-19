19386b383bd07826df8e2e1e91988c8dbeaca325
Shubham Yadav <shubh08am@gmail.com>
2024-01-19 17:37:02 +0530

    Create operator overloading2.cpp

---
+++ operator overloading2.cpp
@@ -0,0 +1,30 @@
+// CPP program to illustrate
+// operators that can be overloaded
+#include <iostream>
+using namespace std;
+
+class integer {
+private:
+	int x;
+
+public:
+    void set(int i){
+        x=i;
+    }
+	
+	integer operator++(int) {
+	    integer i1;
+	    i1.x = x++;
+	    return i1;
+	}
+	void show() { cout << x; }
+};
+
+int main()
+{
+    integer i1,i2;
+    i1.set(3);
+    i2=i1++;
+    i2.show();
+	return 0;
+}
